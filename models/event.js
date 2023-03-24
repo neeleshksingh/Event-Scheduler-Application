1eaf1cd769085170e051f2b86e19f8c88b357c24
neeleshksingh <neelesh1517@gmail.com>
2023-03-24 21:03:18 +0530

    done

--- models/event.js
+++ models/event.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const event = new Schema({
-    title : {type : String, required:true},
-    description : {type : String, required:true},
-    location : {type : String, required:true},
-    startTime : {type : String, required:true},
-    endTime : {type: String, required:true}
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    location: { type: String, required: true },
+    startTime: { type: Date, default: Date.now },
+    endTime: { type: Date, default: Date.now }
 })
 
 const Event = mongoose.model("Event", event)
--- routes/event.js
+++ routes/event.js
@@ -4,148 +4,135 @@ const Event = require('../models/event')
 const router = express.Router()
 
 router.use(express.json())
-router.use(express.urlencoded({extended:false}))
+router.use(express.urlencoded({ extended: false }))
 
-router.post("/", async (req,res)=>{
+router.post("/", async (req, res) => {
     console.log(req.body);
-    const {title, description, location, startTime, endTime} = req.body
-    try{
+    const { title, description, location, startTime, endTime } = req.body
+    try {
         const data = await Event.create({
             title, description, location, startTime, endTime
         })
-        if(data){
+        if (data) {
             return res.status(201).json({
-                status : "success",
+                status: "success",
                 data
             })
         }
-        else{
-            return res.status(400).json({status:"failed"})
+        else {
+            return res.status(400).json({ status: "failed" })
         }
-    } catch(e){
+    } catch (e) {
         return res.status(404).json({
-            error : e.message
+            error: e.message
         })
     }
 })
 
-router.get("/", async(req,res)=>{
-    try{
+router.get("/", async (req, res) => {
+    try {
         const data = await Event.find()
-        if(data){
+        if (data) {
             return res.status(200).json({
                 status: "success",
                 data
             })
         }
-        else{
+        else {
             return res.status(400).json({
                 status: "failed"
             })
         }
-    } catch(e){
+    } catch (e) {
         return res.status(404).json({
-            error : e.message
+            error: e.message
         })
     }
 })
 
-router.get('/:id', async(req,res)=>{
-    try{
-        const data = await Event.findOne({_id:req.params.id})
-        if(data){
+router.get('/:id', async (req, res) => {
+    try {
+        const data = await Event.findOne({ _id: req.params.id })
+        if (data) {
             return res.status(200).json({
                 status: "success",
                 data
             })
         }
-        else{
+        else {
             return res.status(400).json({
                 status: "failed",
-                message : "No event with that id"
+                message: "No event with that id"
             })
         }
-    }catch(e){
+    } catch (e) {
         return res.status(404).json({
-            error : e.message
+            error: e.message
         })
     }
 })
 
-router.delete('/:id', async(req,res)=>{
-    try{
-        const data = await Event.findOne({_id:req.params.id})
-        if(data){
-            const del = await Event.deleteOne({_id:req.params.id})
+router.delete('/:id', async (req, res) => {
+    try {
+        const data = await Event.findOne({ _id: req.params.id })
+        if (data) {
+            const del = await Event.deleteOne({ _id: req.params.id })
             return res.status(204).json({
                 status: "success",
                 del
             })
         }
-        else{
+        else {
             return res.status(400).json({
                 status: "failed",
-                message : "No event with that id"
+                message: "No event with that id"
             })
         }
-    }catch(e){
+    } catch (e) {
         return res.status(404).json({
-            error : e.message
+            error: e.message
         })
     }
 })
 
-router.put('/:id', async(req,res)=>{
-    try{
-        const data = await Event.findOne({_id:req.params.id}) 
-        if(data){
-            const {title, description, location, startTime, endTime} = req.body
-            if(!title){
+router.put('/:id', async (req, res) => {
+    try {
+        const data = await Event.findOne({ _id: req.params.id })
+        if (data) {
+            const { title, description, location, startTime, endTime } = req.body
+            if (!title) {
                 return res.status(400).json({
-                    status : "failed",
-                    error : "Validation error : title is required"
+                    status: "failed",
+                    error: "Validation error : title is required"
                 })
             }
-            if(!description){
+            if (!description) {
                 return res.status(400).json({
-                    status : "failed",
-                    error : "Validation error : description is required"
+                    status: "failed",
+                    error: "Validation error : description is required"
                 })
             }
-            if(!location){
+            if (!location) {
                 return res.status(400).json({
-                    status : "failed",
-                    error : "Validation error : location is required"
+                    status: "failed",
+                    error: "Validation error : location is required"
                 })
             }
-            if(!startTime){
-                return res.status(400).json({
-                    status : "failed",
-                    error : "Validation error : startTime is required"
-                })
-            }
-            if(!endTime){
-                return res.status(400).json({
-                    status : "failed",
-                    error : "Validation error : endTime is required"
-                })
-            }
-
-            const updated = await Event.updateOne({_id:req.params.id}, {...req.body})
+            const updated = await Event.updateOne({ _id: req.params.id }, { ...req.body })
             return res.status(200).json({
                 status: "success",
                 updated
             })
         }
-        else{
+        else {
             return res.status(400).json({
                 status: "failed",
-                message : "No event with that id"
+                message: "No event with that id"
             })
         }
-    }catch(e){
+    } catch (e) {
         return res.status(404).json({
-            error : e.message
+            error: e.message
         })
     }
 })
